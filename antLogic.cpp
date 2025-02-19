1274b40671b7ae330f141b09c29162a1f95914c6
SasZombie <gigelusas81@gmail.com>
2025-02-19 22:58:55 +0200

    commit

--- antLogic.cpp
+++ antLogic.cpp
@@ -1,48 +1,80 @@
 #include <iostream>
 #include "BitReader.hpp"
 
-uint64_t board;
+enum struct Facing{
+    UP, 
+    DOWN,
+    RIGHT,
+    LEFT,
+};
+
+struct Ant
+{
+    size_t x, y;
+    Facing facing;
+};
+
+std::pair<size_t, size_t> move(const Ant& a)
+{
+    switch (a.facing)
+    {
+    case Facing::UP:
+        return std::make_pair(a.x - 1, a.y);
+    
+    case Facing::DOWN:
+        return std::make_pair(a.x + 1, a.y);
+
+    case Facing::LEFT:
+        return std::make_pair(a.x, a.y - 1);
+
+    case Facing::RIGHT:
+        return std::make_pair(a.x, a.y + 1);
+
+    default:
+        std::cerr << "UNREACHABLE with value: " << static_cast<size_t>(a.facing) << '\n';
+        exit(EXIT_FAILURE);
+        break;
+    }
+}
+
+void moveAnt(Ant& ant, BitReader<uint64_t>& bit)
+{
+    size_t index = ant.x * 8 + ant.y;
+
+    if(bit[index] == 0)
+    {
+        ant.facing = static_cast<Facing>((static_cast<size_t>(ant.facing) - 1) % 4);
+    }
+    else
+    {
+        ant.facing = static_cast<Facing>((static_cast<size_t>(ant.facing) + 1) % 4);
+    }
+
+    const auto[x, y] = move(ant);
+
+    ant.x = x;
+    ant.y = y;
+
+    bit[index] = !bit[index];
+}
 
 int main()
 {
-    uint64_t table1 = 0;
-    uint64_t table2 = 0;
-
-    uint64_t value32 = 0b1111111111111111111110000000000;
-    // 111111111111111111111000010000000
-    // 1111111111111111111110000000000
-    //   1111111111111111111110000000000
-    //   10101010101010101010101010101010;
-    //   01010101010101010101010101010101 // Example value for uint32_t
-
+    uint64_t value32 = 0;
+    
     BitReader reader(value32);
+    Ant ant{4, 4, Facing::UP};
 
-    for (size_t i = 0; i < 32; ++i)
-    {
-        std::cout << reader.readValueMSB();
-    }
-
-    std::cout << '\n';
-
-    for (const auto elem : reader)
+    for(const auto elem : reader)
     {
         std::cout << elem;
     }
 
+    moveAnt(ant, reader);
+
     std::cout << '\n';
-
-    reader.setBit(1, 1);
-
-    for (const auto elem : reader)
+    for(const auto elem : reader)
     {
         std::cout << elem;
     }
-    std::cout << '\n';
-    std::cout << reader[2];
-
-    reader[2] = 1;
-
-    std::cout << reader[2];
-
-    std::cout << '\n';
 }
